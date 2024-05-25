a00f887c307eaf04bbaea3eda2c28d687cdf7708
Yossef-Dawoad <yossefdawoad15@gmail.com>
2024-05-25 05:03:50 +0300

    setup localization

--- lib/blog_app.dart
+++ lib/blog_app.dart
@@ -3,6 +3,9 @@ import 'package:clean_blog/core/routes/routes.dart';
 import 'package:clean_blog/core/theme/theme.dart';
 import 'package:flutter/material.dart';
 
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'generated/l10n.dart';
+
 class BlogApp extends StatelessWidget {
   const BlogApp({super.key});
 
@@ -14,6 +17,13 @@ class BlogApp extends StatelessWidget {
       theme: AppTheme.darkThemeData,
       initialRoute: Routes.intitalRoute,
       onGenerateRoute: AppRouter.onGenerateRoute,
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
     );
   }
 }

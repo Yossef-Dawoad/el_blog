f060b972153a9db78cd7410a3c66cf919df0ccf8
Yossef-Dawoad <yossefdawoad15@gmail.com>
2024-05-26 18:47:51 +0300

    implement missing functions in Cloud DB

--- lib/core/common/services/cloud_services/cloud_store_base.dart
+++ lib/core/common/services/cloud_services/cloud_store_base.dart
@@ -1,8 +1,8 @@
 abstract interface class CloudStoreBase<T> {
   Future<T> saveToCloud(T model);
-  Future<T> updateInCloud(T model);
+  Future<T?> updateInCloud(T model);
   Future<List<T>> fetchAllFromCloud();
-  Future<T> fetchFromCloud(String id);
+  Future<T?> fetchFromCloud(String id);
   Future<bool> deleteFromCloud(String id);
   String get collectionPath => '';
 }
--- lib/features/blog/data/datasources/remote/blog_remote_db_store_service.dart
+++ lib/features/blog/data/datasources/remote/blog_remote_db_store_service.dart
@@ -14,36 +14,40 @@ class CloudStoreBlogImpl implements CloudBlogStoreDBBase {
 
   @override
   Future<List<BlogModel>> fetchAllFromCloud() async {
-    final blogs =
-        await _client.from(collectionPath).select('*, profiles (username)');
+    // get all blogs, for each blog fetch it's profile username
+    final blogs = await _client.from(collectionPath).select('*, profiles (username)');
     return blogs
-        .map((blog) => BlogModel.fromMap(blog)
-            .copyWith(author: blog['profiles']['username']))
+        .map((blog) => BlogModel.fromMap(blog).copyWith(author: blog['profiles']['username']))
         .toList();
   }
 
   @override
   Future<BlogModel> saveToCloud(BlogModel model) async {
-    final blogData =
-        await _client.from(collectionPath).insert(model.toMap()).select();
+    final blogData = await _client.from(collectionPath).insert(model.toMap()).select();
     return BlogModel.fromMap(blogData.first);
   }
 
   @override
-  Future<bool> deleteFromCloud(String id) {
-    // TODO: implement deleteFromCloud
-    throw UnimplementedError();
+  Future<bool> deleteFromCloud(String id) async {
+    final response = await _client.from(collectionPath).delete().match({'id': id});
+    //TODO CHeck on this Not Sure if it has .error property
+    if (response.error == null) return true;
+    return false;
   }
 
   @override
-  Future<BlogModel> fetchFromCloud(String id) {
-    // TODO: implement fetchFromCloud
-    throw UnimplementedError();
+  Future<BlogModel?> fetchFromCloud(String id) async {
+    // filter blogs table based on the id and return Single row
+    // Note if Data ever retun more than one row throw EXCEPTION
+    final data = await _client.from(collectionPath).select('id').eq('id', id).maybeSingle();
+    if (data != null) return BlogModel.fromMap(data);
+    return null;
   }
 
   @override
-  Future<BlogModel> updateInCloud(BlogModel model) {
-    // TODO: implement updateInCloud
-    throw UnimplementedError();
+  Future<BlogModel?> updateInCloud(BlogModel model) async {
+    final data = await _client.from(collectionPath).update(model.toMap()).select().maybeSingle();
+    if (data != null) return BlogModel.fromMap(data);
+    return null;
   }
 }
